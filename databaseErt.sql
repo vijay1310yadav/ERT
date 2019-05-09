1f226b74b617b29ba598ab96d1c0dbd324a982c0
Rajat Salvi <rajatsalvi6@gmail.com>
2019-05-09 14:01:57 +0530

    Added Sql Script for table data

---
+++ databaseErt.sql
@@ -0,0 +1,4 @@
+SELECT * FROM employee.employeedetails;
+INSERT INTO employeedetails VALUE(3,"Rajat","Salvi","rajatsalvi6@gmail.com",15-11-1996,"IT");
+select * from employee_details;
+update employeedetails set imageurl="https://upload.wikimedia.org/wikipedia/commons/4/4d/Profile_Picture_for_user_MereTechnicality_%281%29.png" where id=2;
